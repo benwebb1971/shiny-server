95c269986e6176fc3bc39322cf26b1e6f996ce1b
Ben <lpzbsw@Ben-Webb-Droplet1.localdomain>
2017-06-22 10:39:41 +0000

    New reactive method for collecting responses
     with '#' will be ignored, and an empty message aborts the commit.

--- project-pref/app.R
+++ project-pref/app.R
@@ -1,122 +1,178 @@
 library(shiny)
 library(shinythemes)
 
-# which fields get saved 
-# define the fields we want to save from the form
-fieldsAll <- c("name", "studentid", "course", "module", "supervisors")
-
-
-saveData <- function(data) {
-  data <- as.data.frame(t(data))
-  if (exists("responses")) {
-    responses <<- rbind(responses, data)
-  } else {
-    responses <<- data
-  }
-}
-
-loadData <- function() {
-  if (exists("responses")) {
-    responses
-  }
-}
-
-loadSupervisors <- function() {
-  if (exists("supervisors")) {
-    supervisors
-  }
-}
-  
-# directory where responses get stored
+# responses directory
 responsesDir <- file.path("responses")
 
-# directory where responses get stored
-supervisorsDir <- file.path("supervisors")
+saveData <- function(data) {
+  
+  # Create a unique file name
+  fileName <-paste(data[1,1], data[1,2], sep = "-")
+  fileName <-paste(fileName, Sys.Date(), sep = "-")
+  fileName <-paste(fileName, "csv", sep=".")
+  
+  # Write the file to the local system
+  write.csv(
+    x = data,
+    file = file.path(responsesDir, fileName), 
+    row.names = FALSE, quote = TRUE
+  )
+}
 
-# # Load supervisor names and collate in vector
-# setwd("shiny-server/apps/project-pref/supervisors")
-# sname = 'supervisors.csv' # supervisor names
-# supervisors = read.csv(sname,header=FALSE)
-# names = supervisors
-# names <-names[[1]]
-# 
 
+# Mandatory fields for form
+fieldsMandatory <- c("name", "student_id")  # Mandatory fields 
+
+# Fileds we want to save to file
+fields <- c("name", "student_id", "course", "module", "supervisors")
+
+# Star for Mandatory fields 
+labelMandatory <- function(label) {
+  tagList(
+    label,
+    span("*", class = "mandatory_star")
+  )
+}
+
+# Red star and error message
+appCSS <-
+  ".mandatory_star { color: red; }
+#error { color: red; }"
 
 
 shinyApp(
-  ui = fluidPage(
+  ui <- fluidPage(
+    
+    #use shiny js to disable the ID field and CSS for red star
+    shinyjs::useShinyjs(),
+    shinyjs::inlineCSS(appCSS),
+    shinyjs::hidden(
+      div(
+        id = "thankyou_msg",
+        h3("Thanks, your response was submitted successfully!")
+      )),
     
     # Shiny theme
     theme = shinytheme("united"),
-    
-    # Title of ouput
-    titlePanel("Third Year Project Supervisor Preferences"),
-    
-    # Creates a layout with a side bar and main area
-    sidebarLayout(
-      
-      # Input into side bar on the left (in this  case)
-      sidebarPanel(width = 4,
-                   
-                   textInput("name", "Name", "Nobody"),
-                   tags$head(tags$style("#student{color: black;
-                                        font-size: 16px;
-                                        font-name: Arial;
-                                        }")),
-               textInput("studentid", "Student ID", "12345"), # Mandatory fields 
-               tags$head(tags$style("#studentid{color: black;
-                                    font-size: 16px;
-                                    font-name: Arial;
-                                    }")),
-               
-               radioButtons("course", label = h3("Course:"),
-                            choices = list("C800" = "C800", 
-                                           "C850" = "C850")),
-               tags$head(tags$style("#course{color: black;
-                                    font-size: 16px;
-                                    font-name: Arial;
-                                    }")),               
-               
-               radioButtons("module", label = h3("Module:"),
-                            choices = list("Full Project (C83MPR)" = "C83MPR", 
-                                           "Mini-Project (C83MUA)" = "C83MUA", 
-                                           "Mini-Project (C83MUB)" = "C83MUB")),
-               tags$head(tags$style("#module{color: black;
-                                    font-size: 16px;
-                                    font-name: Arial;
-                                    }")),                   
-               selectInput("supervisors", "Choose your 10 preferred supervisors (Delete and move cursor to reorder)",
-                           c("Harriet Allen", "Ben Webb", "Denis Schluppeck"), selected = "Harriet Allen",
-                           selectize = TRUE, multiple = FALSE)),
-      
-      mainPanel(
-        DT::dataTableOutput("responses", width = 300), tags$hr(),
-        actionButton("submit", "Submit", class = "btn-primary"))
-      
-               )
-    ),
+    div(id="form",
+        
+        # Title of ouput
+        titlePanel("Third Year Project Supervisor Preferences"),
+        
+        
+        # Sidebar with a slider input for number of bins 
+        sidebarLayout(
+          sidebarPanel(textInput("name", labelMandatory("Name")),
+                       textInput("student_id",  labelMandatory("Student_ID")),
+                       radioButtons("course", label = h3("Course:"),
+                                    choices = list("C800" = "C800", 
+                                                   "C850" = "C850")),
+                       radioButtons("module", label = h3("Module:"),
+                                    choices = list("Full Project (C83MPR)" = "C83MPR", 
+                                                   "Mini-Project (C83MUA)" = "C83MUA", 
+                                                   "Mini-Project (C83MUB)" = "C83MUB"))),
+          
+          
+          
+          
+          
+          mainPanel(DT::dataTableOutput("table"),
+                    uiOutput("supervisors"),
+                    actionButton("select", "Select Supervisor"),
+                    actionButton("delete", "Delete Supervisor"),
+                    actionButton("submit", "Submit Choices")
+                    
+          )
+        ))),
   
-  
-  server = function(input, output, session) {
+  # Define server logic 
+  server <- function(input, output, session) {
+    
+    # # Load supervisor names and collate in vector
+    sname = 'supervisors.csv' # supervisor names
+    sups = read.csv(sname,header=FALSE)
+    names = c(sups)
+    supervisors <-names[[1]]
+    
+    output$supervisors <- renderUI({
+      selectInput("supervisors", "Choose your 10 preferred supervisors",
+                  supervisors, selected = "",
+                  selectize = TRUE, multiple = FALSE)})
     
     
+    # v$choices will represent the supervisors that have been chosen
+    v <- reactiveValues(choices = c())
     
-    # Whenever a field is filled, x§x§x
-    formData <- reactive({
-      data <- sapply(fieldsAll, function(x) input[[x]])
-      data
+    # When the select button is clicked save the form data, add the chosen supervisor to v$choices
+    # and update the selector
+    observeEvent(input$select, {
+      v$choices <- append(v$choices, input$supervisors)
+      updateSelectInput(session, "supervisors",
+                        choices = supervisors[!(supervisors %in% v$choices)])
     })
-
-    # When a supervisor is chosen, update the form
-    observeEvent(input$supervisors, {
-      saveData(formData())
+    
+    
+    tablevalues <- reactiveValues()
+    tablevalues$df <- data.frame(Name = numeric(), 
+                                 StudentID = numeric(), 
+                                 Course = character(),
+                                 Module = character(),
+                                 Supervisors = character(),
+                                 stringsAsFactors = FALSE)
+    newEntry <- observe({
+      if(input$select > 0) {
+        isolate(tablevalues$df[nrow(tablevalues$df)+1,] <- c(input$name, 
+                                                             input$student_id, 
+                                                             input$course, 
+                                                             input$module, 
+                                                             input$supervisors))
+        
+      }
     })
-
-    # Show the previous responses
-    # (update with current response when Submit is clicked)
-    output$responses <- DT::renderDataTable({
-      input$supervisors
-      loadData()
+    
+    deleteEntry <- observe({
+      if(input$delete > 0) {
+        isolate(tablevalues$df <- tablevalues$df[-nrow(tablevalues$df),])
+      }
     })
+    
+    
+    output$table <- DT::renderDataTable({tablevalues$df}, options = list(paging = FALSE, 
+                                                                         searching = FALSE))
+    
+    
+    # Ensure mandatory fields have input before enabling the submit button
+    observe({
+      mandatoryFilled <-
+        vapply(fieldsMandatory,
+               function(x) {
+                 !is.null(input[[x]]) && input[[x]] != ""
+               },
+               logical(1))
+      mandatoryFilled <- all(mandatoryFilled)
+      
+      
+      # enable/disable the submit button
+      shinyjs::toggleState(id = "submit", condition = mandatoryFilled)
+    })
+    
+    
+    observeEvent(input$submit, {
+      saveData(tablevalues$df)
+      
+      
+      # reset and hide shiny form 
+      shinyjs::reset("form")
+      shinyjs::hide("form")
+      
+      # show thank you message
+      shinyjs::show("thankyou_msg")
+      
+      
+      
+      
+    })
+    
   }
-  )
+)
+
